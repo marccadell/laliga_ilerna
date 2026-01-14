1b2eed082eaa8692b4c81ba6dbf927a987d8f537
SUKEDEV <marcadellfernandez@gmail.com>
2026-01-14 09:24:02 +0100

    Correción de la definición API_KEY en config/config.php y creación/definición de variable SEASON en config/config.php

--- config/config.php
+++ config/config.php
@@ -5,11 +5,13 @@ define('DB_PASS', '');
 define('DB_NAME', 'proyecto-git');
 define('DB_PORT', 3307);
 
-define('API_KEY', 'TU_API_KEY_AQUI');
+define('API_KEY', '123');
 define('LEAGUE_ID', '4335');
+
 
 define('TTL_EQUIPOS', 86400);
 define('TTL_CLASIFICACION', 1800);
 define('TTL_EVENTOS', 21600);
 
 define('ROOT_PATH', 'http://localhost:8080/laliga/public/');
+define('SEASON', '2025-2026');
