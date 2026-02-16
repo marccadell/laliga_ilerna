9bb07dcf9e30500a35b34270bed9daf2d89e8eca
SUKEDEV <marcadellfernandez@gmail.com>
2026-02-16 17:29:03 +0100

    Debugging final de los componentes del proyecto, procedo a realizar merge con la rama main en el siguiente cambio

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../api/api.php';
 //require_once __DIR__ . '/../controllers/sync_clasificacion.php';
 require_once __DIR__ . '/../includes/header.php';
 ?>
-<main class="container">
+<main class="container-index">
 <h1>Bienvenido a LaLiga Stats</h1>
 
 <p>Explora las estadísticas más recientes de La Liga, incluyendo la tabla de clasificación, los próximos partidos y los detalles de cada equipo. Mantente al día con el rendimiento de tus equipos favoritos y no te pierdas ningún partido importante.</p>
